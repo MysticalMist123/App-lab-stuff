3e7d4e41b83ce5eb2238156ba7611efd7ecc6728
MysticalMist123 <70788854+MysticalMist123@users.noreply.github.com>
2023-02-23 14:23:50 +0530

    Add files via upload

---
+++ lab4/q5/script.js
@@ -0,0 +1,22 @@
+const ans = document.getElementById('ans')
+
+for(let i=100;i<=5000;i++){
+    if(isArm(i)){
+        ans.value+=`${i}, `
+    }
+}
+
+function isArm(x){
+    let temp = x
+    let sum = 0
+    while(temp){
+        let digit = temp%10
+        sum+=digit*digit*digit
+        temp = parseInt(temp/10)
+    }
+    // console.log(sum)
+    if(sum == x) return 1
+    return 0
+}
+
+// isArm(153)
